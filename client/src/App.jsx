949bb5674c0191e7e2ea847e591985fbd43ed804
ekimroyrp <ekimroyrp@gmail.com>
2025-11-01 03:28:25 -0700

    Polish aspect selector arrow and text layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -188,8 +188,7 @@ function App() {
               >
                 <span>{selectedRatio.label}</span>
                 <svg
-                  width="12"
-                  height="12"
+                  className="selector-arrow"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
